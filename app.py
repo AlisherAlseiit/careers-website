ef8546697984d7bd4f4f1678b33c9ffad334b1c3
AlisherAlseiit <63951641+AlisherAlseiit@users.noreply.github.com>
2023-05-24 11:42:16 +0000

    updated code

--- app.py
+++ app.py
@@ -1,21 +1,35 @@
 from flask import Flask, render_template, jsonify
-from database import engine, load_jobs_from_db
+from database import engine, load_jobs_from_db, load_job_from_db
 from sqlalchemy import text
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def hello_world():
+def hello_jovian():
   jobs = load_jobs_from_db()
-  return render_template('home.html', jobs=jobs, company_name='Craft')
-
+  return render_template('home.html', 
+                         jobs=jobs)
 
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  
+  if not job:
+    return "Not Found", 404
+  
+  return render_template('jobpage.html', 
+                         job=job)
+
+@app.route("/api/job/<id>")
+def show_job_json(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
+
 
 if __name__ == '__main__':
-  app.run('0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', debug=True)
