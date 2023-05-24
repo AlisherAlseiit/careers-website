8f26e02a09b77a535aed259c51134e4919321fa4
AlisherAlseiit <63951641+AlisherAlseiit@users.noreply.github.com>
2023-05-24 10:20:03 +0000

    added connection to db

--- app.py
+++ app.py
@@ -1,32 +1,20 @@
 from flask import Flask, render_template, jsonify
+from database import engine, load_jobs_from_db
+from sqlalchemy import text
 
 app = Flask(__name__)
-
-JOBS = [{
-  'id': 1,
-  'title': "Data Analyst",
-  'location': 'Almaty, Kazakhstan',
-  'salary': 'KZT, 500,000'
-}, {
-  'id': 2,
-  'title': "Data Scientist",
-  'location': 'Almaty, Kazakhstan',
-}, {
-  'id': 3,
-  'title': "Frontend Engineer",
-  'location': 'Remote',
-  'salary': 'KZT, 550,000'
-}]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html', jobs=JOBS, company_name='Craft')
+  jobs = load_jobs_from_db()
+  return render_template('home.html', jobs=jobs, company_name='Craft')
 
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 
 
 if __name__ == '__main__':
---
+++ database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine, text
+import os 
+
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine(
+  db_connection_string,
+  connect_args={
+    "ssl": {
+      "ssl_ca": "/etc/ssl/cert.pem"
+    }
+  })
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs" ))
+    result_all=result.all()
+    column = result.keys()
+    jobs = []
+    for row in result_all:
+      jobs.append(dict(zip(column,row)))
+    return jobs
+
+  
